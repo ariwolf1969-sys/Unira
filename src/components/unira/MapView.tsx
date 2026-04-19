3ff2ce1ac38e3e712998f345218ae02073f84feb
Ari Wolf <cooperativaunira@gmail.com>
2026-04-19 08:54:00 -0300

    fix: mapa, busqueda destinos, cambiar rol

--- src/components/unira/MapView.tsx
+++ src/components/unira/MapView.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.css';
 
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
